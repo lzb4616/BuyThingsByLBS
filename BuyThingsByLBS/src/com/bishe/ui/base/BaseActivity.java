bdaa83737923ba004f11e2d0ec2d225432a2af76
lzb4616 <lzb4616@gmail.com>
2015-04-22 17:13:51 +0800

    重交一次

--- BuyThingsByLBS/src/com/bishe/ui/base/BaseActivity.java
+++ BuyThingsByLBS/src/com/bishe/ui/base/BaseActivity.java
@@ -10,7 +10,6 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 /**
  * @author robin
