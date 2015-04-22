fd170642cc1b381f46ab97072fd3ffd8cb9fff2f
lzb4616 <lzb4616@gmail.com>
2015-04-22 11:24:56 +0800

    添加下拉刷新第三方库

---
+++ BuyThingsByLBS/src/com/bishe/ui/MainActivity.java
@@ -0,0 +1,12 @@
+package com.bishe.ui;
+
+import com.bishe.ui.base.BaseActivity;
+
+/**
+ * @author robin
+ * @date 2015-4-22
+ * Copyright 2015 The robin . All rights reserved
+ */
+public class MainActivity extends BaseActivity {
+
+}
