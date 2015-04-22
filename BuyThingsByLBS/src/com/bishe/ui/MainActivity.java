2a4e306a9d06ee69335221feb012b3d62af23c6d
lzb4616 <lzb4616@gmail.com>
2015-04-22 17:01:43 +0800

    添加主界面

--- BuyThingsByLBS/src/com/bishe/ui/MainActivity.java
+++ BuyThingsByLBS/src/com/bishe/ui/MainActivity.java
@@ -1,5 +1,7 @@
 package com.bishe.ui;
 
+
+import com.bishe.buythingsbylbs.R;
 import com.bishe.ui.base.BaseActivity;
 
 /**
@@ -8,5 +10,34 @@ import com.bishe.ui.base.BaseActivity;
  * Copyright 2015 The robin . All rights reserved
  */
 public class MainActivity extends BaseActivity {
+
+	@Override
+	public void setContentView() {
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public void initViews() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initListeners() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void fetchData() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- BuyThingsByLBS/src/com/bishe/ui/base/BaseActivity.java
+++ BuyThingsByLBS/src/com/bishe/ui/base/BaseActivity.java
@@ -11,13 +11,13 @@ import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-
+import android.support.v7.app.ActionBarActivity;
 /**
  * @author robin
  * @date 2015-4-20
  * Copyright 2015 The robin . All rights reserved
  */
-public class BaseActivity extends FragmentActivity implements
+public abstract class BaseActivity extends ActionBarActivity implements
 		OnSharedPreferenceChangeListener {
 
 	protected static String TAG ;
@@ -30,9 +30,12 @@ public class BaseActivity extends FragmentActivity implements
 	@Override
 	protected void onCreate(Bundle arg0) {
 		super.onCreate(arg0);
+		setContentView();
 		TAG = this.getClass().getSimpleName();
 		initConfigure();
-		
+		initData();
+		initViews();
+		initListeners();
 	}
 	
 	private void initConfigure() {
@@ -47,6 +50,18 @@ public class BaseActivity extends FragmentActivity implements
 		sputil.getInstance().registerOnSharedPreferenceChangeListener(this);
 		mResources = getResources();
 	}
+	
+	
+	public abstract void setContentView();  
+	  
+    public abstract void initViews();  
+  
+    public abstract void initListeners();  
+  
+    public abstract void initData();  
+    
+    public abstract void fetchData();
+    
 	@Override
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
 			String key) {
